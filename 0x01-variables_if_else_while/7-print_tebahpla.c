9656b300d2d2d61512baf97b2569d41dea94f0f4
Skyseras <yasserelmahfoudi@gmail.com>
2023-02-19 12:00:14 -0800

    done

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
 /**
-*main - Prints the alphabet at reverse
-*
-*Return: Always (Success)
-*/
+ * main - Prints the lowercase alphabet in reverse.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-        char c;
+	char letter;
 
-       for (c = 'z'; c >= 'a'; c--)
-      {
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
 
-          putchar(c);
-    }
-         putchar('\n');
-  return (0);
+	putchar('\n');
+
+	return (0);
 }
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,25 +1,22 @@
 #include <stdio.h>
 
 /**
-*main - Prints a hexadecimal string
-*
-*Return: Always (Success)
-*/
+ * main - Prints all the numbers of base 16 in lowercase.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-   
-     char c;
-  
-   for (c = '0'; c <= '9'; c++)
- 
- {
-           putchar(c);
-}
-    for (c = 'a'; c <= 'f'; c++)
-   {
+	int num;
+	char letter;
 
-     putchar(c);
-}
-     putchar('\n');
-    return (0);
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
 }
