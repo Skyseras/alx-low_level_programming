9889e814ab648b604050857ffccfc5c693801a20
Skyseras <yasserelmahfoudi@gmail.com>
2023-02-19 08:30:34 -0800

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-/*
- * C program that prints exactly "Programming is like building a multilingual puzzle," followed by a new line.
- * */
+
+/**
+ * main - Entry point for programming
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
